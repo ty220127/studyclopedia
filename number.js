a3d5f08ecab5e885315037cd33606d3ec52df6ca
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-15 11:21:13 +0900

    Update number.js

--- number.js
+++ number.js
@@ -14,7 +14,7 @@ i = 0;
 //4:Yamagami2022QWRW
 i++; Y22QWRW = i;
 bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/yamagami2022directivity.html";
+refs[i] = "https://ty220127.github.io/studyclopedia/yamagami2022directivity.html";
 ttls[i] = "Directivity of quantum walk via its random walk replica";
 dates[i] = new Date( 2022, 10, 29, 00, 00 )
 j = 0;
@@ -23,7 +23,7 @@ j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 //5:Amakasu2021CfOAM
 i++; A21OAM = i;
 bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/amakasu2021conflict.html";
+refs[i] = "https://ty220127.github.io/studyclopedia/amakasu2021conflict.html";
 ttls[i] = "Conflict-free collective stochastic decision making by orbital angular momentum of photons through quantum interference";
 dates[i] = new Date( 2021, 10, 26, 00, 00 )
 j = 0;
@@ -32,7 +32,7 @@ j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 //3:SunadaTate2012QW
 i++; S12QW = i;
 bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/sunada2012asymptotic.html";
+refs[i] = "https://ty220127.github.io/studyclopedia/sunada2012asymptotic.html";
 ttls[i] = "Asymptotic behavior of quantum walks on the line";
 dates[i] = new Date( 2012, 3, 15, 00, 00 )
 j = 0;
@@ -41,7 +41,7 @@ j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 //1:Konno2005new
 i++; K05QW = i;
 bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/konno2005new.html";
+refs[i] = "https://ty220127.github.io/studyclopedia/konno2005new.html";
 ttls[i] = "A new type of limit theorem for the one-dimensional quantum random walk";
 dates[i] = new Date( 2005, 10, 00, 00, 00 )
 j = 0;
@@ -50,7 +50,7 @@ j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 //2:Ambainis2001QW1D
 i++; A01QW1D = i;
 bns[i] = [];
-refs[i] = "https://ty220127.github.io/mathemaru/ambainis2001one.html";
+refs[i] = "https://ty220127.github.io/studyclopedia/ambainis2001one.html";
 ttls[i] = "One-dimensional quantum walks";
 dates[i] = new Date( 2001, 07, 00, 00, 00 )
 j = 0;
