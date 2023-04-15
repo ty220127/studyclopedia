a6c199aa45c623540e2564cb4a0654b2a0a48935
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-15 16:35:55 +0900

    Update number.js

--- number.js
+++ number.js
@@ -48,7 +48,7 @@ dates[i] = new Date( 2020, 8, 6, 00, 00 );
 doi[i] = "https://link.springer.com/article/10.1007/s42484-020-00024-8";
 arxiv[i] = "https://arxiv.org/abs/2002.06395";
 j = 0;
-j++; antilinearity = j; bns[i][j] = "報酬平均値";
+j++; antilinearity = j; bns[i][j] = "報酬平均値と成功確率の関係";
 
 //3:SunadaTate2012QW
 i++; S12QW = i;
