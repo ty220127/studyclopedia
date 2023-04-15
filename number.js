046e1ba989c0b9b6a3adcc0154c972ae0221956c
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-15 11:03:47 +0900

    Create number.js

---
+++ number.js
@@ -0,0 +1,50 @@
+//Numbering-Rule
+function nr(model,box) {
+  document.write("[#" + model + "-" + box + "]")
+}
+
+var refs = [];
+var ttls = [];
+var bns = [];
+
+i = 0;
+
+//4:Yamagami2022QWRW
+i++; Y22QWRW = i;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/mathemaru/yamagami2022directivity.html";
+ttls[i] = "Directivity of quantum walk via its random walk replica";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
+//5:Amakasu2021CfOAM
+i++; A21OAM = i;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/mathemaru/amakasu2021conflict.html";
+ttls[i] = "Conflict-free collective stochastic decision making by orbital angular momentum of photons through quantum interference";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
+//3:SunadaTate2012QW
+i++; S12QW = i;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/mathemaru/sunada2012asymptotic.html";
+ttls[i] = "Asymptotic behavior of quantum walks on the line";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
+//1:Konno2005new
+i++; K05QW = i;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/mathemaru/konno2005new.html";
+ttls[i] = "A new type of limit theorem for the one-dimensional quantum random walk";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
+//2:Ambainis2001QW1D
+i++; A01QW1D = i;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/mathemaru/ambainis2001one.html";
+ttls[i] = "One-dimensional quantum walks";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
