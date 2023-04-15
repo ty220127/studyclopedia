15c0e0930b3015a15e62d4cf56e799c5511819b8
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-04-15 13:55:42 +0900

    Update number.js

--- number.js
+++ number.js
@@ -5,8 +5,11 @@ function nr(model,box) {
 
 var refs = [];
 var ttls = [];
+var authors = [];
 var years = [];
 var dates = [];
+var doi = [];
+var arxiv = [];
 var bns = [];
 
 i = 0;
@@ -16,7 +19,10 @@ i++; Y22QWRW = i;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/yamagami2022directivity.html";
 ttls[i] = "Directivity of quantum walk via its random walk replica";
-dates[i] = new Date( 2022, 10, 29, 00, 00 )
+authors[i] = ["Tomoki","Yamagami", "Etsuo","Segawa", "Nicolas","Chauvet", "André","Röhm", "Ryoichi","Horisaki", "Makoto","Naruse"];
+dates[i] = new Date( 2022, 10, 29, 00, 00 );
+doi[i] = "https://www.hindawi.com/journals/complexity/2022/9021583/";
+arxiv[i] = "https://arxiv.org/abs/2201.08067";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 
@@ -25,7 +31,22 @@ i++; A21OAM = i;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/amakasu2021conflict.html";
 ttls[i] = "Conflict-free collective stochastic decision making by orbital angular momentum of photons through quantum interference";
-dates[i] = new Date( 2021, 10, 26, 00, 00 )
+authors[i] = ["Takashi","Amakasu", "Nicolas","Chauvet", "Guillaume","Bachelier", "Serge","Huant", "Ryoichi","Horisaki", "Makoto","Naruse"]; 
+dates[i] = new Date( 2021, 10, 26, 00, 00 );
+doi[i] = "https://www.nature.com/articles/s41598-021-00493-2";
+arxiv[i] = "https://arxiv.org/abs/2107.00877";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
+//6:Casalé2020QB
+i++; C20QB = i;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/studyclopedia/casale2020quantum.html";
+ttls[i] = "Quantum Bandits";
+authors[i] = ["Balthazar","Casalé", "Giuseppe","Di Molfetta", "Hachem","Kadri", "Liva","Ralaivola"]; 
+dates[i] = new Date( 2020, 8, 6, 00, 00 );
+doi[i] = "https://link.springer.com/article/10.1007/s42484-020-00024-8";
+arxiv[i] = "https://arxiv.org/abs/2002.06395";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 
@@ -34,7 +55,10 @@ i++; S12QW = i;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/sunada2012asymptotic.html";
 ttls[i] = "Asymptotic behavior of quantum walks on the line";
-dates[i] = new Date( 2012, 3, 15, 00, 00 )
+authors[i] = ["Toshikazu","Sunada", "Tatsuya","Tate"];
+dates[i] = new Date( 2012, 3, 15, 00, 00 );
+doi[i] = "https://doi.org/10.1016/j.jfa.2011.12.016";
+arxiv[i] = "https://arxiv.org/abs/1108.1878";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 
@@ -43,7 +67,10 @@ i++; K05QW = i;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/konno2005new.html";
 ttls[i] = "A new type of limit theorem for the one-dimensional quantum random walk";
-dates[i] = new Date( 2005, 10, 00, 00, 00 )
+authors[i] = ["Norio","Konno"];
+dates[i] = new Date( 2005, 10, 00, 00, 00 );
+doi[i] = "https://projecteuclid.org/journals/journal-of-the-mathematical-society-of-japan/volume-57/issue-4/A-new-type-of-limit-theorems-for-the-one-dimensional/10.2969/jmsj/1150287309.full";
+arxiv[i] = "https://arxiv.org/abs/quant-ph/0206103";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 
@@ -52,6 +79,8 @@ i++; A01QW1D = i;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/ambainis2001one.html";
 ttls[i] = "One-dimensional quantum walks";
-dates[i] = new Date( 2001, 07, 00, 00, 00 )
+authors[i] = ["Andris","Ambainis", "Eric","Bach", "Ashwin","Nayak", "Ashvin","Vishwanath", "John","Watrous"];
+dates[i] = new Date( 2001, 07, 00, 00, 00 );
+doi[i] = "https://doi.org/10.1145/380752.380757";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
