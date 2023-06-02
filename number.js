709c11beef45da07abb8caad42060bd03deab800
ty220127 <98523610+ty220127@users.noreply.github.com>
2023-06-02 11:47:20 +0900

    Update number.js

--- number.js
+++ number.js
@@ -14,32 +14,8 @@ var bns = [];
 
 i = 0;
 
-//4:Yamagami2022QWRW
-i++; Y22QWRW = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/studyclopedia/yamagami2022directivity.html";
-ttls[i] = "Directivity of quantum walk via its random walk replica";
-authors[i] = ["Tomoki","Yamagami", "Etsuo","Segawa", "Nicolas","Chauvet", "André","Röhm", "Ryoichi","Horisaki", "Makoto","Naruse"];
-dates[i] = new Date( 2022, 10, 29, 00, 00 );
-doi[i] = "https://www.hindawi.com/journals/complexity/2022/9021583/";
-arxiv[i] = "https://arxiv.org/abs/2201.08067";
-j = 0;
-j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
-
-//5:Amakasu2021CfOAM
-i++; A21OAM = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/studyclopedia/amakasu2021conflict.html";
-ttls[i] = "Conflict-free collective stochastic decision making by orbital angular momentum of photons through quantum interference";
-authors[i] = ["Takashi","Amakasu", "Nicolas","Chauvet", "Guillaume","Bachelier", "Serge","Huant", "Ryoichi","Horisaki", "Makoto","Naruse"]; 
-dates[i] = new Date( 2021, 10, 26, 00, 00 );
-doi[i] = "https://www.nature.com/articles/s41598-021-00493-2";
-arxiv[i] = "https://arxiv.org/abs/2107.00877";
-j = 0;
-j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
-
 //6:Casalé2020QB
-i++; C20QB = i;
+i = 6;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/casale2020quantum.html";
 ttls[i] = "Quantum Bandits";
@@ -60,8 +36,32 @@ j++; roP6 = j; bns[i][j] = "命題：反射演算子の別表現と反射性";
 j++; aa6 = j; bns[i][j] = "命題：振幅増幅";
 j++; rp6 = j; bns[i][j] = "命題：QBAIにおける推薦確率";
 
+//5:Amakasu2021CfOAM
+i = 5;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/studyclopedia/amakasu2021conflict.html";
+ttls[i] = "Conflict-free collective stochastic decision making by orbital angular momentum of photons through quantum interference";
+authors[i] = ["Takashi","Amakasu", "Nicolas","Chauvet", "Guillaume","Bachelier", "Serge","Huant", "Ryoichi","Horisaki", "Makoto","Naruse"]; 
+dates[i] = new Date( 2021, 10, 26, 00, 00 );
+doi[i] = "https://www.nature.com/articles/s41598-021-00493-2";
+arxiv[i] = "https://arxiv.org/abs/2107.00877";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
+//4:Yamagami2022QWRW
+i = 4;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/studyclopedia/yamagami2022directivity.html";
+ttls[i] = "Directivity of quantum walk via its random walk replica";
+authors[i] = ["Tomoki","Yamagami", "Etsuo","Segawa", "Nicolas","Chauvet", "André","Röhm", "Ryoichi","Horisaki", "Makoto","Naruse"];
+dates[i] = new Date( 2022, 10, 29, 00, 00 );
+doi[i] = "https://www.hindawi.com/journals/complexity/2022/9021583/";
+arxiv[i] = "https://arxiv.org/abs/2201.08067";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
 //3:SunadaTate2012QW
-i++; S12QW = i;
+i = 3;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/sunada2012asymptotic.html";
 ttls[i] = "Asymptotic behavior of quantum walks on the line";
@@ -72,8 +72,19 @@ arxiv[i] = "https://arxiv.org/abs/1108.1878";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 
+//2:Ambainis2001QW1D
+i = 2;
+bns[i] = [];
+refs[i] = "https://ty220127.github.io/studyclopedia/ambainis2001one.html";
+ttls[i] = "One-dimensional quantum walks";
+authors[i] = ["Andris","Ambainis", "Eric","Bach", "Ashwin","Nayak", "Ashvin","Vishwanath", "John","Watrous"];
+dates[i] = new Date( 2001, 07, 00, 00, 00 );
+doi[i] = "https://doi.org/10.1145/380752.380757";
+j = 0;
+j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
+
 //1:Konno2005new
-i++; K05QW = i;
+i = 1;
 bns[i] = [];
 refs[i] = "https://ty220127.github.io/studyclopedia/konno2005new.html";
 ttls[i] = "A new type of limit theorem for the one-dimensional quantum random walk";
@@ -84,13 +95,3 @@ arxiv[i] = "https://arxiv.org/abs/quant-ph/0206103";
 j = 0;
 j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
 
-//2:Ambainis2001QW1D
-i++; A01QW1D = i;
-bns[i] = [];
-refs[i] = "https://ty220127.github.io/studyclopedia/ambainis2001one.html";
-ttls[i] = "One-dimensional quantum walks";
-authors[i] = ["Andris","Ambainis", "Eric","Bach", "Ashwin","Nayak", "Ashvin","Vishwanath", "John","Watrous"];
-dates[i] = new Date( 2001, 07, 00, 00, 00 );
-doi[i] = "https://doi.org/10.1145/380752.380757";
-j = 0;
-j++; antilinearity = j; bns[i][j] = "第二成分の反線形性";
